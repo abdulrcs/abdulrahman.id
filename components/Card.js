ff60e9c2089ece1b2dd88aa4d9f74b35459262b9
Abdul Rahman <54136956+abdulrcs@users.noreply.github.com>
2021-05-26 09:04:43 +0800

    :hammer: add icons tag

--- components/Card.js
+++ components/Card.js
@@ -8,17 +8,49 @@ import {
   Divider,
   TagLabel,
 } from '@chakra-ui/react'
-import { FaReact, FaPython, FaFlask } from 'react-icons/fa'
+import {
+  FaReact,
+  FaPython,
+  FaPepperHot,
+  FaJs,
+  FaSass,
+  FaCode,
+} from 'react-icons/fa'
 
-export default function Cards({ imageURL, title, desc, projectURL, tag, mt }) {
+export default function Cards({ imageURL, title, desc, projectURL, tag }) {
+  const getTag = (tag) => {
+    let values = []
+    if (tag == 'React') {
+      values[0] = 'blue'
+      values[1] = FaReact
+    } else if (tag == 'Python') {
+      values[0] = 'orange'
+      values[1] = FaPython
+    } else if (tag == 'Javascript') {
+      values[0] = 'yellow'
+      values[1] = FaJs
+    } else if (tag == 'Sass') {
+      values[0] = 'pink'
+      values[1] = FaSass
+    } else if (tag == 'Flask') {
+      values[0] = 'green'
+      values[1] = FaPepperHot
+    } else {
+      values[0] = 'gray'
+      values[1] = FaCode
+    }
+    return values
+  }
+
+  const Tags = tag.map((item) => (
+    <Tag colorScheme={getTag(item)[0]} size="md">
+      <TagLeftIcon as={getTag(item)[1]}></TagLeftIcon>
+      <TagLabel>{item}</TagLabel>
+    </Tag>
+  ))
+
   return (
-    <Stack
-      bg="secondary"
-      borderRadius="10px"
-      mt={{ md: mt }}
-      minH="350px"
-      maxH="500px"
-    >
+    <Stack bg="secondary" borderRadius="10px" minH="350px" maxH="500px">
       <Image
         w="100%"
         src={imageURL}
@@ -29,16 +61,7 @@ export default function Cards({ imageURL, title, desc, projectURL, tag, mt }) {
         <Text fontFamily="Ubuntu" fontSize="2xl" color="white">
           {title}
         </Text>
-        <Stack isInline>
-          <Tag colorScheme="orange" size="md">
-            <TagLeftIcon as={FaFlask}></TagLeftIcon>
-            <TagLabel>Python</TagLabel>
-          </Tag>
-          <Tag colorScheme="blue" size="md">
-            <TagLeftIcon as={FaReact}></TagLeftIcon>
-            <TagLabel>React</TagLabel>
-          </Tag>
-        </Stack>
+        <Stack isInline>{Tags}</Stack>
         <Divider />
         <Text color="textSecondary">{desc}</Text>
       </Stack>
