4b395c0ac9b84839f2a9a3d2acf4a680cfe09979
Abdul Rahman <54136956+abdulrcs@users.noreply.github.com>
2021-07-31 11:38:10 +0800

    :hammer: fix google font

--- pages/_document.js
+++ pages/_document.js
@@ -7,15 +7,15 @@ export default class MyDocument extends NextDocument {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;800&display=swap"
-          rel="preload"
           as="style"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;800&display=swap"
+          rel="stylesheet preload prefetch"
           onload="this.onload=null;this.rel='stylesheet'"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@500&display=swap"
-          rel="preload"
           as="style"
+          href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@500&display=swap"
+          rel="stylesheet preload prefetch"
           onload="this.onload=null;this.rel='stylesheet'"
         />
         <meta name="theme-color" content="black" />
