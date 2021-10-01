80828c7bd04276ca1e23890c00060d0a4c04e1da
Abdul Rahman <54136956+abdulrcs@users.noreply.github.com>
2021-10-01 15:47:47 +0800

    :hammer: fix next.js icon

--- components/Card.js
+++ components/Card.js
@@ -64,7 +64,7 @@ export default function Cards({
       values[1] = FaDatabase
     }
     else if (tag == 'Next.js') {
-      values[0] = 'blue'
+      values[0] = 'gray'
       values[1] = SiNextDotJs
     }
     else {
