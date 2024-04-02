307bb15b29bf263db5e101e6c07f3a2c1df72dbd
abdulrcs <abdulrcs1@gmail.com>
2024-04-02 10:06:26 +0700

    :zap: 2.0.0 - use github issues as posts & add comments feature!
    todo:
    - add pagination after 10 posts
    - add reactions section from post object

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   images: {
-    domains: ['imagizer.imageshack.com', 'i.imgur.com', 'images.unsplash.com', 'media.giphy.com', 'cdn-images-1.medium.com'],
+    domains: [
+      'imagizer.imageshack.com',
+      'i.imgur.com',
+      'images.unsplash.com',
+      'media.giphy.com',
+      'cdn-images-1.medium.com',
+      'github.com',
+    ],
   },
 }
